a87d4e7e2ffc8e819849c338c04922379f54e950
Steffen Jahr <steffen.jahr@thinktecture.com>
2021-09-24 10:49:54 +0200

    EX# 7 & EX#8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,13 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
